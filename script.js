f4069125fcfb73218b11ecad36b171cd8fb9fa29
Yena Kwak <yenak26@gmail.com>
2023-11-12 14:04:39 +0100

    0911 M2-D4

---
+++ script.js
@@ -0,0 +1,135 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+/*
+Primitive Types/  I tipi primitivi rappresentano dati più semplici, con un singolo valore.
+Number:I numeri sono come contare: 1, 2, 3...
+
+String:
+Una stringa è una sequenza di lettere. È come una frase fatta con tante lettere, come "Ciao, tutto bene?".
+
+Boolean:
+Se qualcosa è true o false. es) se chiedo "Piove?", la risposta potrebbe essere true o false.
+
+null:
+Null significa che non c'è niente. È come una scatola vuota.
+
+undefined:
+Undefined significa che qualcosa non è ancora stato deciso. Non c'è una risposta definitiva.
+
+Symbol:
+Un simbolo è come un segno particolare.
+
+
+
+Object Types/ I tipi oggetto, invece, possono contenere informazioni più complesse.
+
+Object: 
+Un oggetto è come una grande scatola dove puoi mettere molte informazioni. Il tuo nome, la tua età, i tuoi colori preferiti possono essere messi lì dentro.
+
+Array:
+Un array è come una lista di cose in fila. Ad esempio, immagina una scatola di giocattoli con tutti i giocattoli allineati
+
+Function:
+Una funzione è come una scatola magica che fa qualcosa di speciale quando la chiami. Le chiedi di fare qualcosa e lei lo fa
+
+
+*/
+
+
+
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+/* un oggetto è una variabile speciale che può contenere molte informazioni.
+Immagina un oggetto come una scatola magica che può avere diversi scomparti.
+Ogni scomparto contiene un pezzo di informazione, ed ogni pezzo è etichettato con una chiave speciale.
+Gli oggetti sono uno strumento potente in JavaScript per organizzare e manipolare dati in modo flessibile.
+
+*/
+
+
+
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+/* 4
+console.log(5+13);
+*/
+
+
+
+
+
+
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+/*
+ let X = 12;
+console.log(X);
+*/
+
+
+
+
+
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+/* 
+let name = "Yena" 
+*/
+
+
+
+
+
+
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+/* 
+let X = 12;
+let theResult = 4-X;
+console.log(theResult); 
+*/
+
+
+
+
+
+
+
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+
+ 
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* 
+let name1 = "john";
+let name2 = "John";
+console.log(name1 === name2);
+
+console.log(name1.toLowerCase() === name2.toLowerCase());
+
+*/
